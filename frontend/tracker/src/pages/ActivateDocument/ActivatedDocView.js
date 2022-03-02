c63cbfb9dcff95f8ac7b498ead06608fef5ccf84
Richard Arthur <richardarthur228@gmail.com>
2022-03-02 09:15:53 +0000

    small changes

---
+++ frontend/tracker/src/pages/ActivateDocument/ActivatedDocView.js
@@ -0,0 +1,149 @@
+import { Box, Text } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Redirect } from "react-router-dom";
+import Loading from "../../components/Loading/Loading";
+import { fetchDocument } from "../../http/document";
+import pdf from "../../assets/images/pdf-img.png";
+import { useStateValue } from "../../store/StateProvider";
+import Preview from "../../components/Preview/Preview";
+
+function ActivateDocView() {
+  const [store, dispatch] = useStateValue();
+  const [openPreview, setOpenPreview] = useState(false);
+  const [document, setDocument] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const activatedDoc = store.activatedDocumentDetails;
+
+  useEffect(() => {
+    _fetchDocument(store.token);
+  }, [activatedDoc]);
+
+  const _fetchDocument = async () => {
+    const res = await fetchDocument(store.token, activatedDoc?.document.id);
+    const data = res.data;
+    setDocument(data);
+    setLoading(false);
+  };
+
+  if (!activatedDoc) {
+    return <Redirect to="/" />;
+  }
+
+  const handlePreview = () => {
+    setOpenPreview(!openPreview);
+  };
+  return (
+    <>
+      {!loading ? (
+        <Box>
+          <Box
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            maxWidth="900px"
+            overflowX="auto"
+            whiteSpace="nowrap"
+          >
+            <Text
+              fontWeight="600"
+              marginRight="20px"
+              textTransform="capitalize"
+              color="var(--dark-brown)"
+              _hover={{ cursor: "pointer" }}
+              fontSize="15px"
+              padding="3px"
+              bg="var(--lighter-brown)"
+              borderRadius="50px"
+            >
+              {document.subject}
+            </Text>
+            {document?.related_document.map((doc) => {
+              return (
+                <Text
+                  key={doc.id}
+                  onClick={() => handlePreview(doc)}
+                  fontWeight="600"
+                  marginRight="20px"
+                  textTransform="capitalize"
+                  color="var(--dark-brown)"
+                  _hover={{ cursor: "pointer" }}
+                  fontSize="16px"
+                >
+                  {doc.subject}
+                </Text>
+              );
+            })}
+          </Box>
+          <Box marginTop="20px" display="flex" justifyContent="space-around">
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              flex="0.3"
+            >
+              <Box
+                h="270px"
+                w="250px"
+                marginTop="10px"
+                backgroundColor="var(--lightest-brown)"
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                cursor="pointer"
+                borderRadius="10px"
+                onClick={() => handlePreview(document)}
+              >
+                <img
+                  src={pdf}
+                  alt="logo"
+                  className="file-preview-box-img"
+                  style={{ width: "80%", opacity: "0.7" }}
+                />
+              </Box>
+              <Box
+                display="flex"
+                flexDirection="column"
+                width="200px"
+                margin="auto"
+                marginTop="20px"
+              >
+                {openPreview && (
+                  <Preview setOpenPreview={setOpenPreview} doc={document} />
+                )}
+              </Box>
+            </Box>
+            <div className="vr vr-sm"></div>
+
+            <div className="file-info">
+              <div
+                className={`minute-box-preview 
+                  "minute-box-preview-lg"
+                `}
+              >
+                <div>
+                  {document?.minute?.map((item) => {
+                    return (
+                      <div className="minute" key={item?.id}>
+                        <p>{item?.content}</p>
+                        <p className="employee">{item?.user}</p>
+                        <p className="date">
+                          Date: {new Date(item?.date).toDateString()}
+                        </p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </Box>
+        </Box>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
+}
+
+export default ActivateDocView;
