1c536898db25c1ac6cfa2175dd3b6923f9953e40
Richard Arthur <richardarthur228@gmail.com>
2022-03-12 14:28:02 +0000

    updated app.js

--- frontend/tracker/src/pages/App.js
+++ frontend/tracker/src/pages/App.js
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
 
-import { Route, Switch } from "react-router-dom";
-import { loadUser } from "../http/user";
-import { useStateValue } from "../store/StateProvider";
-import PrivateRoute from "../utility/PrivateRoute";
-import Dashboard from "./Dashboard/Dashboard";
-import * as actionTypes from "../store/actionTypes";
-
 import Login from "./Login/Login";
+import { loadUser } from "../http/user";
+import Dashboard from "./Dashboard/Dashboard";
+import { Route, Switch } from "react-router-dom";
+import PrivateRoute from "../utility/PrivateRoute";
+import * as actionTypes from "../store/actionTypes";
+import { useStateValue } from "../store/StateProvider";
 
 function App() {
   const [store, dispatch] = useStateValue();
