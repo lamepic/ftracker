ed07c3370367af086cf7a7d9ecee5e2df0a8e66e
Richard Arthur <richardarthur228@gmail.com>
2022-03-23 09:20:51 +0000

    small changes

--- frontend/tracker/src/hooks/useIcon.js
+++ frontend/tracker/src/hooks/useIcon.js
@@ -3,8 +3,6 @@ import docIcon from "../assets/icons/doc.svg";
 import xlsIcon from "../assets/icons/xls.svg";
 import pdfIcon from "../assets/icons/pdf.svg";
 import pptIcon from "../assets/icons/ppt.svg";
-import folderIcon from "../assets/icons/folder.svg";
-import multipleFilesIcon from "../assets/icons/multiple-files.svg";
 
 const icons = {
   doc: docIcon,
