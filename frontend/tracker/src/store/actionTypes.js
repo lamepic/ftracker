a2ea8909b093b7d6cb3468293fc7f13083998b52
Richard Arthur <richardarthur228@gmail.com>
2022-05-09 07:44:07 +0000

    updated actionTypes

--- frontend/tracker/src/store/actionTypes.js
+++ frontend/tracker/src/store/actionTypes.js
@@ -1,9 +1,9 @@
 // Auth actionTypes
 export const AUTH_ERROR = "AUTH_ERROR";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const SET_TOKEN = "SET_TOKEN";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
-export const USER_LOADED = "USER_LOADED";
+export const SET_USER = "SET_USER";
 
 // Document actionTypes
 export const SET_INCOMING_COUNT = "SET_INCOMING_COUNT";
